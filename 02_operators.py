67539c9f938afa74f46d268f5572932e0483803c
franmidev <franmirad@hotmail.com>
2024-06-06 11:53:17 -0400

    operadores en python

--- 02_operators.py
+++ 02_operators.py
@@ -1,2 +1,27 @@
 ### Operators ###
 
+# Arithmetic Operations in Python
+# Integers
+
+print('Addition: ', 1 + 2)        # 3
+print('Subtraction: ', 2 - 1)     # 1
+print('Multiplication: ', 2 * 3)  # 6
+print ('Division: ', 4 / 2)       # 2.0  Division in Python gives floating number
+print('Division: ', 6 / 2)        # 3.0         
+print('Division: ', 7 / 2)        # 3.5
+print('Division without the remainder: ', 7 // 2)   # 3,  gives without the floating number or without the remaining
+print ('Division without the remainder: ',7 // 3)   # 2
+print('Modulus: ', 3 % 2)         # 1, Gives the remainder
+print('Exponentiation: ', 2 ** 3) # 9 it means 2 * 2 * 2
+print('#######################################################')
+
+# Example: Floats
+
+# Floating numbers
+print('Floating Point Number, PI ', 3.14)
+print('Floating Point Number, gravity ', 9.81)
+print('#######################################################')
+
+# Complex numbers
+print('Complex number: ', 1 + 1j)
+print('Multiplying complex numbers: ', (1 + 1j) * (1 - 1j))
