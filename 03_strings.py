0af22d05b1a899f6ffe9141e7efaf03eda4e1bfb
Francisco Miranda <164576996+FranMiDev@users.noreply.github.com>
2024-06-07 23:03:18 -0400

    strings

--- 03_strings.py
+++ 03_strings.py
@@ -55,3 +55,68 @@ last_name = 'Miranda'
 languaje = 'Python'
 formated_string = 'I am %s %s. I teach %s' %(first_name, last_name, languaje)
 print(formated_string)
+
+# Strings and numbers
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+formated_string = 'The area of circle with a radius %d is %.2f.' %(radius, area) # 2 refers the 2 sifnigicant digits after the point
+print(formated_string)
+
+python_libraries = ['Django', 'Flask', 'NumPy', 'Matplotlib', 'Pandas']
+formated_string = 'The following are python libraries: %s' %(python_libraries)
+print(formated_string)
+
+# New Style String Formatting (str.format)
+
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+languaje = 'Python'
+formated_string = 'I am {} {}. I teach {}'.format(first_name, last_name, languaje)
+print(formated_string)
+
+a = 4
+b = 3
+
+print('{} + {} = {}'.format(a, b, a+b))
+print('{} - {} = {}'.format(a, b, a-b))
+print('{} * {} = {}'.format(a, b, a*b))
+print('{} / {} = {:.2f}'.format(a, b, a/b)) # limits it to two digits after decimal
+print('{} % {} = {}'.format(a, b, a % b))
+print('{} // {} = {}'.format(a, b, a // b))
+print('{} ** {} = {}'.format(a, b, a ** b))
+
+# Strings and numbers
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+formated_string = 'The area of a circle with a radius {} is {:.2f}.'.format(radius, area) # 2 digits a after decimal
+print(formated_string)
+
+# String Interpolation /f-Strings(Python 3.6+)
+
+a = 4
+b = 3
+print(f'{a} + {b} = {a +b}')
+print(f'{a} - {b} = {a - b}')
+print(f'{a} * {b} = {a * b}')
+print(f'{a} / {b} = {a / b:.2f}')
+print(f'{a} % {b} = {a % b}')
+print(f'{a} // {b} = {a // b}')
+print(f'{a} ** {b} = {a ** b}')
+print('---------------------------------------------------------')
+
+# Python Strings as Sequences of Characters
+# Unpacking Characters
+
+language = 'Python'
+a,b,c,d,e,f = language # unpacking sequence characters into variables
+print(a) # p
+print(b) # y
+print(c) # t
+print(d) # h
+print(e) # o
+print(f) # n
+
+# Accesssing Characters in Strings by index
+
