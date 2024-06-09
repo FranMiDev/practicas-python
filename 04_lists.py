c22466c851a406e9136a1ba317834cf42e5098d8
Francisco Miranda <164576996+FranMiDev@users.noreply.github.com>
2024-06-09 14:00:52 -0400

    empezando la clase lits

---
+++ 04_lists.py
@@ -0,0 +1,24 @@
+### LISTS ###
+
+# Using list built-in function
+# Syntax
+lst = list()
+
+empty_list = list() # this is an empty list, no item in the list
+print(len(empty_list)) # 0
+print('-----------------------------------------------------')
+
+# Using square brackets, []
+# syntax
+lst = []
+
+empty_list = []
+print(len(empty_list))
+print('-----------------------------------------------------')
+
+# lists with initial values. We use len() to find length of a list
+fruits = ['banana', 'orange', 'mango', 'lemon']                         # list of fruits
+vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']         # list of vegetables
+animal_products = ['milk', 'meat', 'butter', 'youghurt']                # list of animal products
+web_techs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongoDB']  # list of web technologies
+countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
